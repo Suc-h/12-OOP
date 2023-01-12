e2f804e7aced4075a1a74235a555f7db3fe9c45b
Suc-h <97514706+Suc-h@users.noreply.github.com>
2023-01-12 13:12:32 +0100

    Základ
    Not Done

---
+++ 11oop/Produkt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11oop
+{
+    class Produkt : Zbozi
+    {
+        bool lzezlevnit;
+        public Produkt(bool zlezlevnit,string nazev, int cena, DateTime datumVyroby, int minTrvanlivost) : base(nazev, cena, datumVyroby, minTrvanlivost)
+        {
+            this.lzezlevnit = lzezlevnit;
+        }
+
+        public override double CenaDPH()
+        {
+            
+            if(Splnuje()==true)
+            {
+                double cenadphlater = base.CenaDPH() / 100;
+                double cenadphnow = cenadphlater * 60;
+                return cenadphnow;
+            }
+           /*else
+            {
+                double cenadphlater = base.CenaDPH() / 100;
+                double cenadphnow = cenadphlater * 60;
+                return cenadphnow;
+            }*/
+            
+        }
+    }
+}
---
+++ 11oop/Zbozi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11oop
+{
+    class Zbozi
+    {
+        string nazev;
+        int cena;
+        DateTime datumVyroby;
+        int minTrvanlivost;
+
+        public Zbozi(string nazev, int cena, DateTime datumVyroby, int minTrvanlivost)
+        {
+            this.nazev = nazev;
+            this.cena = cena;
+            this.datumVyroby = datumVyroby;
+            this.minTrvanlivost = minTrvanlivost;
+        }
+
+        public bool Splnuje()
+        {
+            TimeSpan dny = DateTime.Now - datumVyroby;
+            if (dny.TotalDays > minTrvanlivost)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public virtual double CenaDPH()
+        {
+            double a = cena / 100;
+            double cenadph = a * 121;
+            return cenadph;
+        }
+
+        public override string ToString()
+        {
+            if (Splnuje() == true)
+            {
+                return nazev + "s datumem výroby:" + datumVyroby + "s cenou: " + cena + " s cenou s DPH: " + CenaDPH() + " nesplňuje minimální trvanlivost ";
+            }
+            else
+            {
+                return nazev + "s datumem výroby:" + datumVyroby + "s cenou: " + cena + " s cenou s DPH: " + CenaDPH() + " splňuje minimální trvanlivost ";
+            }
+        }
+    }
+}
